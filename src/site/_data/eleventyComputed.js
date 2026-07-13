31355efff39847275fe583ab1cff0389390473fe
LytTheBit <102967110+LytTheBit@users.noreply.github.com>
2026-07-13 22:06:27 +0200

    Add title and hideTitle properties to eleventyComputed

--- src/site/_data/eleventyComputed.js
+++ src/site/_data/eleventyComputed.js
@@ -6,5 +6,10 @@ module.exports = {
   graph: async (data) => await getGraph(data),
   filetree: (data) => getFileTree(data),
   userComputed: (data) => userComputed(data),
-  noteProps: (data) => data["dg-note-properties"]
+  noteProps: (data) => data["dg-note-properties"],
+  title: (data) =>
+    Object.prototype.hasOwnProperty.call(data, "dg-page-title")
+      ? data["dg-page-title"]
+      : undefined,
+  hideTitle: (data) => data["dg-page-title"] === ""
 };
